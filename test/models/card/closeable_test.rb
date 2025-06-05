eb95af4add6ddb37f907a9109a3b79a5ed450d74
Jorge Manrubia <jorge@37signals.com>
2025-06-05 13:29:34 +0200

    Add test for reopening cards

--- test/models/card/closeable_test.rb
+++ test/models/card/closeable_test.rb
@@ -6,16 +6,29 @@ class Card::CloseableTest < ActiveSupport::TestCase
   end
 
   test "closed scope" do
-    assert_equal [ cards(:shipping) ], Card.closed
+    assert_equal [cards(:shipping)], Card.closed
     assert_not_includes Card.open, cards(:shipping)
   end
 
-  test "popping" do
+  test "close cards" do
     assert_not cards(:logo).closed?
 
-    cards(:logo).close(user: users(:kevin))
+    assert_difference -> { cards(:logo).events.count }, +1 do
+      cards(:logo).close(user: users(:kevin))
+    end
 
     assert cards(:logo).closed?
+    assert cards(:logo).events.last.action.card_closed?
     assert_equal users(:kevin), cards(:logo).closed_by
+  end
+
+  test "reopen cards" do
+    assert cards(:shipping).closed?
+
+    assert_difference -> { cards(:shipping).events.count }, +1 do
+      cards(:shipping).reopen
+    end
+    assert cards(:shipping).reload.open?
+    assert cards(:shipping).events.last.action.card_reopened?
   end
 end
