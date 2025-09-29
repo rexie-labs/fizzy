ea03116ad09637befdf209bb2e23f493ca0dcb74
Jorge Manrubia <jorge@hey.com>
2025-09-29 18:53:01 +0200

    Remove unused job

--- app/jobs/card/auto_reconsider_all_stagnated_job.rb
+++
@@ -1,7 +0,0 @@
-class Card::AutoReconsiderAllStagnatedJob < ApplicationJob
-  def perform
-    ApplicationRecord.with_each_tenant do |tenant|
-      Card.auto_reconsider_all_stagnated
-    end
-  end
-end
